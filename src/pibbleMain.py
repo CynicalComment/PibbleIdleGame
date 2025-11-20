f12a1450269c319e904c74680ee74434f6a8965d
Edward Lujan <edwardllujans@gmail.com>
2025-11-20 17:26:47 -0600

    coding with russ mouse event tutorial

--- src/pibbleMain.py
+++ src/pibbleMain.py
@@ -2,16 +2,21 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-
+clicked = False
 run = True
 while run:
 
+    if (pygame.mouse.get_pressed()[0]) == True:
+        print("left mouse click")
+    if (pygame.mouse.get_pressed()[2]) == True:
+        print("Right mouse click")
+
     for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print(event)
         if event.type == pygame.QUIT:
             run = False
+
+pygame.quit()

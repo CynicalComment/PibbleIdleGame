df33f6347005daa18a1c6e2d0536d9096038d89d
Edward Lujan <edwardllujans@gmail.com>
2025-12-08 23:07:29 -0600

    made 5 states for pibble cleanliness yes im rushin to get this done in an hour :,D

--- src/pibbleMain.py
+++ src/pibbleMain.py
@@ -1,22 +1,25 @@
 import pygame
 
 pygame.init()
-
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 400
-
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 clicked = False
-run = True
-while run:
-
-    if (pygame.mouse.get_pressed()[0]) == True:
-        print("left mouse click")
-    if (pygame.mouse.get_pressed()[2]) == True:
-        print("Right mouse click")
-
+clock = pygame.time.Clock()
+running = True
+while running:
+    mouse_pos = pygame.mouse.get_pos()
+    #events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            running = False
+
+    screen.fill('black')
+
+    pygame.draw.circle(screen, "red", mouse_pos,20)
+    #RENDER HERE
+    pygame.display.flip()
+
+    clock.tick(120)
 
 pygame.quit()

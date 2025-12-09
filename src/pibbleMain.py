5a02b22bcc653eea9f9b75103c629db796a42870
Edward Lujan <edwardllujans@gmail.com>
2025-12-08 23:39:19 -0600

    made a smaller rect for collision drawing circle after png boolean to check for collision with cursor

--- src/pibbleMain.py
+++ src/pibbleMain.py
@@ -18,8 +18,11 @@ PibbleState = [
 time_elapsed = 0
 time_cleaning_elapsed = 0
 
+isCleaning = False
 sprite = PibbleState[0]
-rect = sprite.get_rect(center=(200,200))
+rect = sprite.get_rect(center=(SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+collision_rect = rect.inflate(-50, -100)
+
 running = True
 while running:
     mouse_pos = pygame.mouse.get_pos()
@@ -28,21 +31,26 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    if rect.collidepoint(mouse_pos):
+    if collision_rect.collidepoint(mouse_pos):
+        isCleaning = True
         time_cleaning_elapsed += 1
-        if time_cleaning_elapsed >= 10:
+        if time_cleaning_elapsed >= 100:
             time_elapsed = (time_elapsed + 1) % 5
             sprite = PibbleState[time_elapsed]
             rect = sprite.get_rect(center=rect.center)
             time_cleaning_elapsed = 0
     else:
         time_cleaning_elapsed = 0
+        isCleaning = False
             
 
     screen.fill('black')
 
-    pygame.draw.circle(screen, "pink", mouse_pos,10)
+    
     screen.blit(sprite,rect)
+    #screen.blit(pygame.draw.circle(screen,'pink',collision_rect,20),collision_rect)
+    if isCleaning:
+        pygame.draw.circle(screen, "pink", mouse_pos,10)
     #RENDER HERE
     pygame.display.flip()
 
